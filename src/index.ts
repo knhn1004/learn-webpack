ee9caf02e09bff3ed320c066ada923e1afb7d24d
oliver <oliver@pop-os.localdomain>
2021-03-27 16:48:32 +0800

    setup babel loader

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
-function log(str: string) {
-  console.log(str)
+export function log(str: string) {
+  console.log(str);
 }
 
-log('hello world');
+class A {
+  greeting = 'Hello World!';
+}
+
+log(new A().greeting);
